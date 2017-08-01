513873b7ad1dd4224c8f8f4af0d33c7b8107199b
Mike Hansford <mjhansford@gmail.com>
2017-08-02 00:05:40 +1000

    cmder config

---
+++ cmder/user-profile.ps1
@@ -0,0 +1,37 @@
+﻿# Use this file to run your own startup commands
+
+## Prompt Customization
+<#
+.SYNTAX
+    <PrePrompt><CMDER DEFAULT>
+    Î» <PostPrompt> <repl input>
+.EXAMPLE
+    <PrePrompt>N:\Documents\src\cmder [master]
+    Î» <PostPrompt> |
+#>
+
+[ScriptBlock]$PrePrompt = {
+}
+
+# Replace the cmder prompt entirely with this.
+[ScriptBlock]$CmderPrompt = {
+    $id = 1
+    $historyItem = Get-History -Count 1
+    if ($historyItem)
+    {
+        $id = $historyItem.Id + 1
+    }
+
+    write-host "PS [$id] > " -NoNewline -foregroundcolor yellow
+    write-host $pwd -foregroundcolor darkcyan -NoNewLine
+
+}
+
+# [ScriptBlock]$PostPrompt = {
+
+# }
+
+## <Continue to add your own>
+
+set-alias repeat invoke-history
+
