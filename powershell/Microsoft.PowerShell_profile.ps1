c0771c8dab851563ffb48a2d90055043e1e0daec
Mike Hansford <mjhansford@gmail.com>
2021-03-09 22:24:20 +1100

    Add PSH profile

---
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,16 @@
+﻿
+Import-Module 'C:\tools\poshgit\dahlbyk-posh-git-9bda399\src\posh-git.psd1'
+
+function prompt {
+    $path = $ExecutionContext.SessionState.Path.CurrentLocation.Path
+
+    $HistoryId = $MyInvocation.HistoryId
+
+    Write-Host
+    Write-Host "$env:COMPUTERNAME " -ForegroundColor DarkGray -NoNewline
+    Write-Host $path -ForegroundColor DarkGreen
+    Write-host "[$HistoryId] " -ForegroundColor DarkYellow -NoNewline
+    return "> "
+}
+
+Set-Alias repeat Invoke-History
